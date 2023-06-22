db7f3cef66637d6f0a0228337111c50d4948c9ce
azcat01 <azcat01@yahoo.co.id>
2023-06-23 02:40:20 +0700

    final revision

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build -t azcat01/karsajobs:latest .
+docker build -t azcat01/karsajobs:latest . # build Dockerfile
 
-docker tag azcat01/karsajobs:latest ghcr.io/azcat01/karsajobs:latest
+docker tag azcat01/karsajobs:latest ghcr.io/azcat01/karsajobs:latest # change the image's name
 
-echo $CR_PAT | docker login ghcr.io -u azcat01 --password-stdin
+echo $CR_PAT | docker login ghcr.io -u azcat01 --password-stdin # login to github with token
 
-docker push ghcr.io/azcat01/karsajobs:latest
+docker push ghcr.io/azcat01/karsajobs:latest # push the image to github packages
